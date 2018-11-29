158bcb079a7e3e68493e938d08b7aaf77681af67
Alexey Kondratyev <alx.kond@gmail.com>
2018-11-29 19:30:31 +0300

    Files terraform\main.tf terraform\outputs.tf terraform\variables.tf been changed

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,61 +3,85 @@ provider "google" {
   project = "${var.project}"
   region  = "${var.region}"
 }
-
-resource "google_compute_project_metadata_item" "default" {
-  key   = "ssh-key"
-  value = "appuser:${file(var.public_key_path)}appuser2:${file(var.public_key_path)}"
+module "app" {
+ source = "modules/app"
+ public_key_path = "${var.public_key_path}"
+ zone = "${var.zone}"
+ app_disk_image = "${var.app_disk_image}"
 }
-
-resource "google_compute_instance" "app" {
-  name         = "${format("reddit-app%02d", count.index+1)}"
-  machine_type = "g1-small"
-  zone         = "${var.zone}"
-  count        = 2
-
-  boot_disk {
-    initialize_params {
-      image = "${var.disk_image}"
-    }
-  }
-
-  network_interface {
-    network       = "default"
-    access_config = {}
-  }
-
-  metadata {
-    ssh-keys = "appuser:${file(var.private_key_path)}"
-  }
-
-  tags = ["reddit-app"]
-
-  connection {
-    type        = "ssh"
-    user        = "appuser"
-    agent       = false
-    private_key = "${file(var.private_key_path)}"
-  }
-
-  provisioner "file" {
-    source      = "files/puma.service"
-    destination = "/tmp/puma.service"
-  }
-
-  provisioner "remote-exec" {
-    script = "files/deploy.sh"
-  }
+module "db" {
+ source = "modules/db"
+ public_key_path = "${var.public_key_path}"
+ zone = "${var.zone}"
+ db_disk_image = "${var.db_disk_image}"
 }
+# resource "google_compute_project_metadata_item" "default" {
+#   key   = "ssh-key"
+#   value = "appuser:${file(var.public_key_path)}appuser2:${file(var.public_key_path)}"
+# }
+# resource "google_compute_instance" "app" {
+#   name         = "${format("reddit-app%02d", count.index+1)}"
+#   machine_type = "g1-small"
+#   zone         = "${var.zone}"
+#   count        = 3
 
-resource "google_compute_firewall" "firwall_puma" {
-  name    = "allow-puma-default"
-  network = "default"
+#   boot_disk {
+#     initialize_params {
+#       image = "${var.disk_image}"
+#     }
+#   }
 
-  allow {
-    protocol = "tcp"
-    ports    = ["9292"]
-  }
+#   network_interface {
+#     network       = "default"
+#     access_config = {}
+#   }
 
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["reddit-app"]
-}
+#   metadata {
+#     ssh-keys = "appuser:${file(var.public_key_path)}"
+#   }
+
+#   tags = ["reddit-app"]
+
+#   connection {
+#     type        = "ssh"
+#     user        = "appuser"
+#     agent       = false
+#     private_key = "${file(var.private_key_path)}"
+#   }
+
+#   provisioner "file" {
+#     source      = "files/puma.service"
+#     destination = "/tmp/puma.service"
+#   }
+
+#   provisioner "remote-exec" {
+#     script = "files/deploy.sh"
+#   }
+# }
+
+# resource "google_compute_firewall" "firwall_puma" {
+#   name    = "allow-puma-default"
+#   network = "default"
+
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["9292"]
+#   }
+
+#   source_ranges = ["0.0.0.0/0"]
+#   target_tags   = ["reddit-app"]
+# }
+# resource "google_compute_firewall" "firewall_ssh" {
+#   name    = "default-allow-ssh"
+#   network = "default"
+
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["22"]
+#   }
+
+#   source_ranges = ["0.0.0.0/0"]
+# }
+# resource "google_compute_address" "app_ip" {
+#   name = "reddit-app-ip"  
+# }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,10 @@
-output "loadbalancer_ip" {
-  value = "${google_compute_global_forwarding_rule.global_forwarding_rule_reddit_app.ip_address}"
+# output "loadbalancer_ip" {
+#   value = "${google_compute_global_forwarding_rule.global_forwarding_rule_reddit_app.ip_address}"
+# }
+# output "app_external_ip" {
+#   value = "${google_compute_instance.app.network_interface.0.access_config.0.assigned_nat_ip}"
+# }
+
+output "app_external_ip" {
+ value = "${module.app.app_external_ip}"
 }
