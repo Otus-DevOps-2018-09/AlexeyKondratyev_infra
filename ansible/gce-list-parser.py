88fbef1e35dd158d73e03fd1e39019d1725868b9
Alexey Kondratyev <alx.kond@gmail.com>
2018-12-08 15:14:02 +0300

    Dynamic inventory files added

---
+++ ansible/gce-list-parser.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import sys, json
+inventory = {'all':{'host':{}, 'children':{}}}
+
+instances=json.load(sys.stdin)
+for instance in instances:
+    host_name = instance['name']
+    host_ip = instance['networkInterfaces'][0]['accessConfig'][0]['natIP']
+    tags = instance['tags']['items']
+
+    host = {'ansible_host':host_ip}
+    inventory['all']['hosts'][host_name] = host
+
+    for tag in tags:
+        if tag != host_name:
+            if tag not in inventory['all']['children']:
+                invenotory['all']['children'][tag] = {'hosts':{}}
+            inventory['all']['children'][tag] = {'hosts':{}}
+
+print json.dumps(inventory)
